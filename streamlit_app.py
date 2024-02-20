3db12e8ebf290053b1f55fbbdeed04a1b2ae08ab
moomooclub <160581087+moomooclub@users.noreply.github.com>
2024-02-19 23:54:50 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,3 +47,24 @@ map_data = pd.DataFrame(
     columns=['lat', 'lon'])
 
 st.map(map_data)
+
+
+# Using Widget
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
+
+st.text_input("Your name", key="name")
+
+# You can access the value at any point with:
+st.session_state.name
+
+# Checkbox to show/ hide data
+st.write("""
+# Checkbox to show / hide data
+""")
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    chart_data
