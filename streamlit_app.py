5f8f08cc7d9132435b592ab72cbe63667aaae524
moomooclub <160581087+moomooclub@users.noreply.github.com>
2024-02-19 23:48:02 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
+import pandas as pd
 
 st.write("""
 # My first app
 Hello *world!*
 """)
+
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
