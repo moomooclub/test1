055b47150ba928e2b0f9285d2fcc5b4a85adde90
moomooclub <160581087+moomooclub@users.noreply.github.com>
2024-02-19 23:50:16 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,34 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 st.write("""
 # My first app
 Hello *world!*
 """)
 
+# Table
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 40]
 })
 
 df
+
+# Random NP
+dataframe = np.random.randn(10, 20)
+st.dataframe(dataframe)
+
+# Chart Data
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+# Plot a map
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
